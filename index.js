f23cdf06f8d1bb3612635e693a6ea799fc9ad34d
Elliot Kim <elliotkim916@gmail.com>
2019-05-01 16:37:21 -0700

    Added setInterval and setDate function

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.min-hand');
+const secondHand = document.querySelector('.sec-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;  
+  // we take our seconds, divide that by 60 to get a percentage, and multiply that by 360 to get the value that is the percentage of 360
+  // for example, if seconds is 59, then 59/60 = .9833, then multiplying .9833 by 360 gives us the value that is .9833 of 360 degrees
+  console.log(seconds);
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 24) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
